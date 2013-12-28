f6b79de5266341290748ace124bf3a470b1db342
os-notepc <water_totuka@yahoo.co.jp>
2013-12-29 02:58:32 +0900

    歩兵ボタンを下に

--- Scene.cpp
+++ Scene.cpp
@@ -37,7 +37,7 @@ GameScene::GameScene(){
 
 	//addLayer(5,std::make_shared<MapLayer>(game));
 	addLayer(4,std::make_shared<MenuLayer>(game));
-	addLayer(4,std::make_shared<HOHEILayer>(game,164,4));
+	addLayer(4,std::make_shared<HOHEILayer>(game,164,7));
 	//addLayer(10,std::make_shared<MapLayer>(game));
 	notEnd=true;
 	old_stage=1;
