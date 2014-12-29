701ddcc109c833665690e4554de9d8ac3dad6a9d
os-notepc <water_totuka@yahoo.co.jp>
2014-12-29 22:57:05 +0900

    stage6が出ないバグの修正

--- Scene.cpp
+++ Scene.cpp
@@ -146,7 +146,7 @@ void WorldScene::enterScene(){
 	int tmp[6][2]={
 		{76,288},{497,53},{817,330},{1125,94},{1286,377},{1600,222}
 	};
-	for(int i=1;i<6;i++){
+	for(int i=1;i<=6;i++){
 		if(Data::getIns()->get("stage")+1<i)break;
 		shared_ptr<GraphicLayer> q((new ButtonLayer(tmp[i-1][0],tmp[i-1][1],Images::get("pic/world_stage.png"),0,0,275,98))->setId("stage"+to_string(i))->setEnterSE("sound/button03a.mp3")->setClickSE("sound/se_maoudamashii_system49.wav"));
 		addLayer(8,q); 
